fe4206750481adbce4485adb313258d8590d3772
Apple <86308689+Appleeyes@users.noreply.github.com>
2022-10-26 19:55:27 +0100

    Add files via upload

---
+++ userAuthMySQL/students.sql
@@ -0,0 +1,16 @@
+
+
+
+  
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `full_names`, `country`, `email`, `gender`, `password`) VALUES
+(2, 'Seyi Olufe', 'Nigeria', 'seyi@gmail.com', 'Male', '1234'),
+(3, 'Chioma Victoria', 'Nigeria', 'vicky@gmail.com', 'Female', '129323'),
+(4, 'Nfon Andrew', 'Cameroon', 'drew@gmail.com', 'Male', 'tatah'),
+(8, 'Samuel Dorcas', 'Nigeria', 'samueldorcaseshowokeoghene@gmail.com', 'Female', 'Dorcas'),
+(9, 'Peter Samuel', 'South Africa', 'peter@gmail.com', 'Male', 'peter'),
+(10, 'Peter Oni', 'Cameroon', 'oni@gmail.com', 'Male', 'dfyre');
+
